c921749b43e00a2b7f4ac6fc4a2a4d6ca71e3bee
sunilsGubbala <154649760+sunilsGubbala@users.noreply.github.com>
2023-12-23 23:03:15 +0530

    js changes

--- js/app.js
+++ js/app.js
@@ -20,8 +20,10 @@ init();
 function navRelated(){
     const navbarContainer=document.querySelector(".nav-container");
     window.addEventListener("scroll",function(){
+
         const scrollNumber = window.scrollY;
         const targetNumber = window.innerWidth>=992 ? 70:50;
+
         if (scrollNumber >= targetNumber){
             navbarContainer.classList.add("sticky-nav");
         } else{
@@ -30,3 +32,37 @@ function navRelated(){
     });
 }
 navRelated();
+
+
+
+
+function submitForm() {
+    // Get values from the form
+    var name = document .getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        alert('Invalid email Address');
+        return;
+    }
+
+    if (message.trim() === '') {
+        alert('Please enter a Message');
+        return;
+    }
+
+    if (name.trim() === ''){
+        alert('Please enter a Name');
+        return;
+    }
+
+
+    alert("form submitted succesfully");
+
+    document.getElementById ("email").value ="";
+    document.getElementById("message").value="";
+    document.getElementById('name').value="";
+    
+};
